f2c2830095df9c0f1576ef7779cfcdea0ffbc23d
Ujvari Gyorgy <ujvari.gyorgy@gmail.com>
2026-01-06 19:24:42 +0200

    lightrays

--- event-app/app/page.tsx
+++ event-app/app/page.tsx
@@ -1,5 +1,10 @@
 const page = () => {
-  return <div>page</div>;
+  return (
+    <section>
+      <h1 className="text-center">Szekelyudvarhelyi csocso klub</h1>
+      <p className="text-center mt-5">Csocso csocso csocos</p>
+    </section>
+  );
 };
 
 export default page;
