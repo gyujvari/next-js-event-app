d0868e9f0d2e6aa2d49c2802fd37e1265fc013a9
Ujvari Gyorgy <ujvari.gyorgy@gmail.com>
2026-01-07 18:55:20 +0200

    navbar , event cards

--- event-app/app/page.tsx
+++ event-app/app/page.tsx
@@ -1,8 +1,25 @@
+import EventCard from "@/components/EventCard";
+import ExploreBtn from "@/components/ExploreBtn";
+
+const events = [
+  { image: "/images/event1.png", title: "Event 1" },
+  { image: "/images/event2.png", title: "Event 2" },
+];
+
 const page = () => {
   return (
     <section>
-      <h1 className="text-center">Szekelyudvarhelyi csocso klub</h1>
+      <h1 className="text-center">Goaldiggers</h1>
       <p className="text-center mt-5">Csocso csocso csocos</p>
+      <ExploreBtn />
+      <div className="mt-20 space-y-7">
+        <h3>Featured events</h3>
+        <ul className="events">
+          {events.map((event) => (
+            <EventCard {...event} />
+          ))}
+        </ul>
+      </div>
     </section>
   );
 };
