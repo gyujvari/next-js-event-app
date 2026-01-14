4ad3458f0ebef108d196ae8bd0aea859b002aafd
Ujvari Gyorgy <ujvari.gyorgy@gmail.com>
2026-01-14 12:38:08 +0200

    upload images and get request

--- event-app/app/api/events/route.ts
+++ event-app/app/api/events/route.ts
@@ -1,6 +1,7 @@
 import { connectToDatabase } from "@/lib/mongodb";
 import { NextRequest, NextResponse } from "next/server";
 import { Event } from "@/database/event.model";
+import { v2 as cloudinary } from "cloudinary";
 
 export async function POST(req: NextRequest) {
   try {
@@ -17,6 +18,33 @@ export async function POST(req: NextRequest) {
         { status: 400 }
       );
     }
+
+    const file = formData.get("image") as File;
+
+    if (!file) {
+      return NextResponse.json(
+        { message: "Image file is requires" },
+        { status: 400 }
+      );
+    }
+
+    const arrayBuffer = await file.arrayBuffer();
+    const buffer = Buffer.from(arrayBuffer);
+
+    const uploadResult = await new Promise((resolve, reject) => {
+      cloudinary.uploader
+        .upload_stream(
+          { resource_type: "image", folder: "DevEvent" },
+          (error, results) => {
+            if (error) return reject(error);
+            resolve(results);
+          }
+        )
+        .end(buffer);
+    });
+
+    event.image = (uploadResult as { secure_url: string }).secure_url;
+
     const createdEvent = await Event.create(event);
   } catch (e) {
     console.error(e);
@@ -28,6 +56,24 @@ export async function POST(req: NextRequest) {
       {
         status: 500,
       }
+    );
+  }
+}
+
+export async function GET() {
+  try {
+    await connectToDatabase();
+
+    const events = await Event.find().sort({ createdAt: -1 });
+
+    return NextResponse.json(
+      { message: "Fetched successfully", events },
+      { status: 200 }
+    );
+  } catch (e) {
+    return NextResponse.json(
+      { message: "Failed tp fetch evenets" },
+      { status: 400 }
     );
   }
 }
