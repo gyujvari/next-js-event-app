b17cd373f8129a2daeb3a7e01afb16cb22cada0f
Ujvari Gyorgy <ujvari.gyorgy@gmail.com>
2026-01-14 12:08:06 +0200

    events-api

---
+++ event-app/app/api/events/route.ts
@@ -0,0 +1,33 @@
+import { connectToDatabase } from "@/lib/mongodb";
+import { NextRequest, NextResponse } from "next/server";
+import { Event } from "@/database/event.model";
+
+export async function POST(req: NextRequest) {
+  try {
+    await connectToDatabase();
+
+    const formData = await req.formData();
+
+    let event;
+    try {
+      event = Object.fromEntries(formData.entries());
+    } catch (e) {
+      return NextResponse.json(
+        { message: "Invalid JSON data format" },
+        { status: 400 }
+      );
+    }
+    const createdEvent = await Event.create(event);
+  } catch (e) {
+    console.error(e);
+    return NextResponse.json(
+      {
+        message: "Event creation failed",
+        error: e instanceof Error ? e.message : "Unknown",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
